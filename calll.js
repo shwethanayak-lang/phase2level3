4404f6ca55a6452e7be1d263050cad610a59277a
Shwetha Nayak <64678982+shwethanayak-lang@users.noreply.github.com>
2021-04-14 16:27:57 +0530

    comments inserted

--- calll.js
+++ calll.js
@@ -1,11 +1,13 @@
+//function for clear button
 $("#clear").on("click", function () {
   $("#screen").val(" ");
 });
 
+// function for equal
 $("#equal").on("click", function () {
   $("#screen").val(eval($("#screen").val()));
 });
-
+// function for delete button
 var del = () => {
   value = $("#screen").val();
   $("#screen").val(value.substring(0, value.length - 1));
